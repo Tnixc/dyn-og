85abd666b7c711665cf80dbfe79d2a209ebf8b21
Tnixc <tnixxc@gmail.com>
2024-09-15 01:09:21 -0400

    implement dyn title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
 import Head from 'next/head'
 
 export default function Page() {
+  const title = "Vercel Edge Network" // You can make this dynamic if needed
+  const encodedTitle = encodeURIComponent(title)
+
   return (
     <div>
       <Head>
-        <meta name="og:title" content="Vercel Edge Network" />
-        <meta name="og:description" content="Vercel Edge Network" />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={title} />
         <meta
           name="og:image"
           content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
-              process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/vercel`
+            `${process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
+            }/api/vercel?title=${encodedTitle}`
           }
         />
       </Head>
